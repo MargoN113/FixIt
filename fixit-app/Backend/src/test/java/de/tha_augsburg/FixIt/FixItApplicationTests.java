01c250a47c40bb2db514e9b438807d2cc3bc22df
Margarita Nefedova <margarita.nefedova@tha.de>
2025-04-19 20:31:05 +0200

    Ordner und Datein für die Spring Boot Konfiguration

---
+++ fixit-app/Backend/src/test/java/de/tha_augsburg/FixIt/FixItApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tha_augsburg.FixIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
