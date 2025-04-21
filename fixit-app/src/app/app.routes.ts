74922aa00bc0842b7bcb5ef40cdb92f568738eef
Dominik Kohler <kohler.dominik@gmx.de>
2025-04-21 17:16:22 +0200

    feat: initial commit of react setup and initialisation

--- fixit-app/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import {MapComponent} from './pages/map/map.component';
-import { AboutComponent } from './pages/about/about.component';
-
-export const routes: Routes = [
-  { path: '', component: MapComponent },
-  { path: 'map', component: MapComponent },
-  { path: 'about', component: AboutComponent},
-];
